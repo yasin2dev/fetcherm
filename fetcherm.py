b127a7edd74e7db7564a884459f855ebdb517210
yasin2dev <yasin.bozkurt.n@gmail.com>
2024-03-28 00:54:26 +0300

    Win32 support for fetcherm

--- fetcherm.py
+++ fetcherm.py
@@ -1,5 +1,6 @@
-from libs.filerm import filerm
+from libs.filerm import filerm, filermWindows
 from libs.color import *
+import platform
 
 def fetcherm():
     printlnWithBold(TGRN, "\nWelcome to Fetcherm")
@@ -9,16 +10,26 @@ def fetcherm():
     for i in range(len(f"[{filerm.user}@{filerm.host}]")):
         print("-", end='')
 
-    printWithBold(TYLW, f"\nOS:  {TWHT}{filerm.getOS('os-name')}")
-    printWithBold(TYLW, f"Kernel: {TWHT}{filerm.getOS('kernel')}")
-    printWithBold(TYLW, f"Shell: {TWHT}{filerm.getOS('shell')}")
-    printWithBold(TYLW, f"DE: {TWHT}{filerm.getOS('desktop-env')}")
-    printWithBold(TYLW, f"WM: {TWHT}{filerm.getOS('window-mngr').capitalize()}")
-    printWithBold(TYLW, f"Theme: {TWHT}{filerm.ReadTheme()}")
 
-    printWithBold(TYLW, f"CPU: {TWHT}{filerm.ReadCPU()}")
-    printWithBold(TYLW, f"GPU: {TWHT}{filerm.ReadGPU()}")
-    printWithBold(TYLW, f"Memory: {TWHT}{filerm.CurrentRam() + ' / ' + filerm.ReadMemory('mb')}")
-    printWithBold(TYLW, f"Disk size: {TWHT}{filerm.ReadDisk()}\n")
+    if platform.system() == "Linux":
+        printWithBold(TYLW, f"\nOS:  {TWHT}{filerm.getOS('os-name')}")
+        printWithBold(TYLW, f"Kernel: {TWHT}{filerm.getOS('kernel')}")
+        printWithBold(TYLW, f"Shell: {TWHT}{filerm.getOS('shell')}")
+        printWithBold(TYLW, f"DE: {TWHT}{filerm.getOS('desktop-env')}")
+        printWithBold(TYLW, f"WM: {TWHT}{filerm.getOS('window-mngr').capitalize()}")
+        printWithBold(TYLW, f"Theme: {TWHT}{filerm.ReadTheme()}")
+
+        printWithBold(TYLW, f"CPU: {TWHT}{filerm.ReadCPU()}")
+        printWithBold(TYLW, f"GPU: {TWHT}{filerm.ReadGPU()}")
+        printWithBold(TYLW, f"Memory: {TWHT}{filerm.CurrentRam() + ' / ' + filerm.ReadMemory('mb')}")
+        printWithBold(TYLW, f"Disk size: {TWHT}{filerm.ReadDisk()}\n")
+    elif platform.system() == "Windows":
+        printWithBold(TYLW, f"\nOS: {TWHT} Windows {filermWindows.getOs("os-name")}")
+        printWithBold(TYLW, f"Version: {TWHT}{filermWindows.getOs("version")}")
+
+        printWithBold(TYLW, f"CPU: {TWHT}{filermWindows.ReadCPU()}")
+        printWithBold(TYLW, f"GPU: {TWHT}{filermWindows.ReadGPU()}")
+        printWithBold(TYLW, f"RAM: {TWHT}{filermWindows.ReadMemory("gb") + " GB"}")
+        printWithBold(TYLW, f"Disk size: {TWHT}{filerm.ReadDisk()}\n")
     
 fetcherm()
